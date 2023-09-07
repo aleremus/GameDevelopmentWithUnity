695e5b9b9047dfb42e0e428b6e9f8899b0c2b142
Ekaterina Urmanova <aleremualeremus@gmail.com>
2023-09-07 18:50:49 +0300

    base control + Sample Scene
    Camera Movement
    Walk
    Jump
    
    Simple maze

---
+++ GDU/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    [Range(0, 1000)] public float mouseSensitivity = 100;
+
+    [SerializeField] float minVerticalView = 25f;
+    [SerializeField] float maxVerticalView = 85f;
+    [SerializeField] Transform player;
+
+    private float _Xrotation = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Vector2 mouse = new Vector2(
+            Input.GetAxis("Mouse X"),
+            Input.GetAxis("Mouse Y")) * Time.deltaTime * mouseSensitivity;
+        _Xrotation -= mouse.y;
+        _Xrotation = Mathf.Clamp(_Xrotation, -maxVerticalView, minVerticalView);
+
+        transform.localRotation = Quaternion.Euler(_Xrotation, 0, 0);
+
+        player.Rotate(mouse.x * Vector3.up);
+
+    }
+}
---
+++ GDU/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Vector2 moveInput;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private Rigidbody _playerRigidbody;
+    [SerializeField] private int jumpAmount;
+
+    private int _jumpLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _jumpLeft = jumpAmount;
+
+        if (!_playerRigidbody)
+            _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var velocity = new Vector3(
+            moveInput.x * playerSpeed,
+            _playerRigidbody.velocity.y,
+            moveInput.y * playerSpeed);
+        _playerRigidbody.velocity = transform.TransformDirection(velocity);
+    }
+
+    void OnJump()
+    {
+        if (_jumpLeft <= 0)
+            return;
+
+        _jumpLeft--;
+
+        _playerRigidbody.velocity = new Vector3(
+            _playerRigidbody.velocity.x,
+            jumpForce,
+            _playerRigidbody.velocity.z);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _jumpLeft = jumpAmount;
+    }
+}
